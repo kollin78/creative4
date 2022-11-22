11f837d35bea24ae68f20a954fe88baa97771342
Kollin <kollroge78@gmail.com>
2022-11-22 18:05:03 +0000

    small changes

--- src/App.js
+++ src/App.js
@@ -9,79 +9,82 @@ function App() {
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
 
-  const fetchQuotes = async() => {
-    try {      
+  const fetchQuotes = async () => {
+    try {
       const response = await axios.get("/api/quotes");
       setQuotes(response.data.quotes);
-    } catch(error) {
+    }
+    catch (error) {
       setError("error retrieving quotes: " + error);
     }
   }
 
-  const createQuote = async() => {
+  const createQuote = async () => {
     try {
-      await axios.post("/api/quotes", {quote: quote, author: author});
-    } catch(error) {
+      await axios.post("/api/quotes", { quote: quote, author: author });
+    }
+    catch (error) {
       setError("error adding a quote: " + error);
     }
   }
 
-    // fetch ticket data
-    useEffect(() => {
-      fetchQuotes();
-    },[]);
-  
-    const addQuote = async(e) => {
-      e.preventDefault();
-      await createQuote();
-      fetchQuotes();
-      setQuote("");
-      setAuthor("");
-    }
+  // fetch ticket data
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
 
-    const deleteOneQuote = async(quote) => {
-      try {
-        await axios.delete("/api/quotes/" + quote.author);
-      } catch(error) {
-        setError("error deleting a quote" + error);
-      }
+  const addQuote = async (e) => {
+    e.preventDefault();
+    await createQuote();
+    fetchQuotes();
+    setQuote("");
+    setAuthor("");
+  }
+
+  const deleteOneQuote = async (quote) => {
+    try {
+      await axios.delete("/api/quotes/" + quote.author);
     }
-  
-  
-    const deleteQuote = async(quote) => {
-      await deleteOneQuote(quote);
-      fetchQuotes();
+    catch (error) {
+      setError("error deleting a quote" + error);
     }
+  }
+
+
+  const deleteQuote = async (quote) => {
+    await deleteOneQuote(quote);
+    fetchQuotes();
+  }
 
   return (
     <div className="App">
-      {error}
-      <h1>Create a Memorable Quote</h1>
-      <form onSubmit={addQuote}>
-        <div>
-          <label>
-            Quote:
-            <input type="text" value={quote} onChange={e => setQuote(e.target.value)} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Author:
-            <textarea value={author} onChange={e=>setAuthor(e.target.value)}></textarea>
-          </label>
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
-      <h1>Quotes</h1>
-      {quotes.map( quote => (
-        <div key={quote.author} className="ticket">
-          <div className="problem">
-            <p>{quote.quote}</p>
-            <p><i>-- {quote.author}</i></p>
+      <div class="flex-box">
+        {error}
+        <h1>Create a Memorable Quote</h1>
+        <form onSubmit={addQuote}>
+          <div class="input">
+            <label class="label">
+              Quote: <input type="text" value={quote} onChange={e => setQuote(e.target.value)} />
+            </label>
           </div>
-          <button onClick={e => deleteQuote(quote)}>Delete</button>
-        </div>
+          <div class="input">
+            <label class="label">
+              Author: <input value={author} onChange={e=>setAuthor(e.target.value)}></input>
+            </label>
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+        <h1>Quotes</h1>
+        {quotes.map( quote => (
+          <div key={quote.author} className="ticket">
+            <div className="problem">
+              <p>"{quote.quote}"</p>
+              <p><i>-- {quote.author}</i></p>
+            </div>
+            <button onClick={e => deleteQuote(quote)}>Delete</button>
+          </div>
       ))}     
+      </div>
     </div>
   );
 }
